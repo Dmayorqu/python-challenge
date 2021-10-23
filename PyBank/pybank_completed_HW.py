7f864c37437b13a0ce9117de3b874c1ab78dfcf1
Dmayorqu <85854145+Dmayorqu@users.noreply.github.com>
2021-10-23 09:31:44 -0700

    Add files via upload

---
+++ PyBank/pybank_completed_HW.py
@@ -0,0 +1,38 @@
+import csv
+totalmonths=0
+Totalprofit =0
+changelist=[]
+Month=[]
+
+
+
+with open('Resources/budget_data.csv') as data:
+    reader=csv.reader(data)
+    header=next(reader)
+    firstrow=next(reader)
+    totalmonths+=1
+    Totalprofit+=int(firstrow[1])
+    previous=int(firstrow[1])
+
+    for row in reader:
+        totalmonths+=1
+        Totalprofit+=int(row[1])
+        change=int(row[1])-previous
+        previous=int(row[1])
+        changelist.append(change)
+        Month.append(row[0])
+
+averagechange=sum(changelist)/len(changelist)
+minchange=min(changelist)
+maxchange=max(changelist)
+minchangeindex=changelist.index(minchange)
+maxchangeindex=changelist.index(maxchange)
+minmonth=Month[minchangeindex]
+maxmonth=Month[maxchangeindex]
+
+print(f"Average change: {averagechange}, min change {minmonth}: {minchange}, max change {maxmonth}: {maxchange}")
+
+
+
+
+    
